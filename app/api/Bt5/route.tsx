9b7560a2b412350b1770a7a545e57d61c719bb4c
dungphamrk <147359437+dungphamrk@users.noreply.github.com>
2024-09-04 09:29:59 +0700

    1

--- app/api/Bt5/route.tsx
+++ app/api/Bt5/route.tsx
@@ -1,4 +1,5 @@
 export default async function GET(){
+    console.log(1);
     const data = await fetch(" https://fakestoreapi.com/products")
     .then((res)=>res.json())
     .catch((err)=>console.log(err))
